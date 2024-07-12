62882785be1dfc35735b7bf4909401e0f5af1487
GL <grace.lopez1222@icloud.com>
2024-07-12 13:57:11 -0500

    Initial Commit

---
+++ 12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  12
+//
+//  Created by Grace Lopez on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 12/_2App.swift
@@ -0,0 +1,17 @@
+//
+//  _2App.swift
+//  12
+//
+//  Created by Grace Lopez on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct _2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
